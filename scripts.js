f01d645a5902d867b2d22635783c81f5cdb9d509
Lorena Sarasua <lorena.sarasua@gmail.com>
2021-03-28 21:07:59 -0400

    Bonus Mission: rocket remains inside background area, moves in direction clicked. returns to original postion when landing or aborted. height in miles displayed accordingly.

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
 
-function init() {
+function pageLoaded() {
   const takeOffButton = document.getElementById('takeoff');
   const flightStatus = document.getElementById('flightStatus');
   const shuttleBackground = document.getElementById('shuttleBackground');
@@ -13,62 +13,98 @@ function init() {
   const rightButton = document.getElementById('right');
   const leftButton = document.getElementById('left');
   const rocket = document.getElementById('rocket');
+  const startPosition = rocket.style.inset = '250px 0px 0px 0px';  /* top right bottom left */
+  // const rocketMove = document.getElementById('rocketMove');
 
   takeOffButton.addEventListener('click', function() {
-    console.log('take off!');
     let shouldTakeOff = window.confirm('Confirm that the shuttle is ready for takeoff:');
     if (shouldTakeOff) {
       flightStatus.innerHTML = 'Shuttle in flight.';
       shuttleBackground.style.backgroundColor = 'blue';
-      spaceShuttleHeight.innerHTML = '10,000';
+      spaceShuttleHeight.innerHTML = 10000;
+      rocket.style.inset = '240px 0px 0px 0px';
     }
   });
 
   landingButton.addEventListener('click', function() {
-    console.log('landing!');
     let isLanding = window.alert('The shuttle is landing. Landing gear engaged.');
     flightStatus.innerHTML = 'The shuttle has landed.';
     shuttleBackground.style.backgroundColor = 'green';
-    spaceShuttleHeight.innerHTML = '0';
+    spaceShuttleHeight.innerHTML = 0;
+    rocket.style.inset = startPosition;
   });
 
   missionAbortButton.addEventListener('click', function() {
-    console.log('abort!');
     let shouldAbort = window.confirm('Confirm that you want to abort the mission.');
     if (shouldAbort) {
       flightStatus.innerHTML = 'Mission aborted.';
       shuttleBackground.style.backgroundColor = 'green';
-      spaceShuttleHeight.innerHTML = '0';
+      spaceShuttleHeight.innerHTML = 0;
+      rocket.style.inset = startPosition;
     }
   });
 
+  /* 
+  Use top and left only
+  top max: 250px (will be at bottom of background)
+  top min: 0px (will be at top of background)
+  left max: 130px (will be at right of background)
+  left min: -130px (will be at left of background) 
+  */
+
   upButton.addEventListener('click', function() {
     let location = Number((rocket.style.top).replace('px',''));
-    let moveTo = (location -= 10) + 'px';
-    console.log('up: ' + moveTo);
-    rocket.style.top = moveTo;
+    let height = Number(spaceShuttleHeight.innerHTML);
+    if (location > 0) {
+      let moveTo = (location -= 10) + 'px';
+      rocket.style.top = moveTo;
+      height += 10000;
+      spaceShuttleHeight.innerHTML = height;
+    }
   });
 
   downButton.addEventListener('click', function() {
     let location = Number((rocket.style.top).replace('px',''));
-    let moveTo = (location += 10) + 'px';
-    console.log('down: ' + moveTo);
-    rocket.style.top = moveTo;
+    let height = Number(spaceShuttleHeight.innerHTML);
+    if (location < 250) {
+      let moveTo = (location += 10) + 'px';
+      rocket.style.top = moveTo;
+      height -= 10000;
+      spaceShuttleHeight.innerHTML = height;      
+    }  
   });  
 
   rightButton.addEventListener('click', function() {
     let location = Number((rocket.style.left).replace('px',''));
-    let moveTo = (location += 10) + 'px';
-    console.log('right: ' + moveTo);
-    rocket.style.left = moveTo;
+    if (location < 130) {    
+      let moveTo = (location += 10) + 'px';
+      rocket.style.left = moveTo;
+    }
   });
 
   leftButton.addEventListener('click', function() {
     let location = Number((rocket.style.left).replace('px',''));
-    let moveTo = (location -= 10) + 'px';
-    console.log('left: ' + moveTo);
-    rocket.style.left = moveTo;
-  });  
+    if (location > -130) {      
+      let moveTo = (location -= 10) + 'px';
+      rocket.style.left = moveTo;
+    }
+  });
+  
+  // rocketMove.addEventListener('click', function(event) {
+  //   // console.log(startPosition);
+  //   let direction = event.target.id;
+  //   console.log(direction);
+  //   let coordinates = [250, 0, 0, 0];
+  //   if (direction === 'up') {
+  //     coordinates[0] -= 10;
+  //   } else if (direction === 'down') {
+  //     coordinates[0] += 10;
+  //   }
+  //   console.log(coordinates);
+  // });
 }
 
-window.addEventListener('load', init);
+window.addEventListener('load', pageLoaded);
+
+
+
